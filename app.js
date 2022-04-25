4204dae18b0be2e5a718a4d6832c2c77d2b2b4cb
tanvinpavel <tanvinpavel123@gmail.com>
2022-04-26 02:42:37 +0600

    yooda hostel 1.0.1v

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 //route method
 const app = express();
 
-const whiteList = ['http://127.0.0.1:5050', 'http://localhost:3000'];
+const whiteList = ['http://127.0.0.1:5050', 'http://localhost:3000', 'https://quizzical-brahmagupta-875937.netlify.app'];
 
 const axiosOption = {
     origin: (origin, callback) => {
