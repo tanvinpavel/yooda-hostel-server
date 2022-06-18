0c7ff4de3d39cbdc69ea7d63272c5c6d7e837822
tanvinpavel <tanvinpavel123@gmail.com>
2022-06-18 13:09:15 +0600

    cors problem

--- Config/corsOptions.js
+++ Config/corsOptions.js
@@ -2,9 +2,11 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || true){
+        if(allowedOrigins.indexOf(origin) !== -1){
+            console.log('incoming origin', origin);
             callback(null, true);
         }else{
+            console.log('incoming origin', origin);
             callback(new Error('not allow by cors'))
         }
     },
