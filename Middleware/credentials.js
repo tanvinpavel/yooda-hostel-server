84b0bad99b9b52133f34a5f3d4a6bf7e05461456
tanvinpavel <tanvinpavel123@gmail.com>
2022-06-18 13:30:38 +0600

    cors creadentials

--- Middleware/credentials.js
+++ Middleware/credentials.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('../Config/allowedOrigins');
 
 const credentials = (req, res, next) => {
     const origin = req?.headers?.origin;
-    if(allowedOrigins.includes(origin)){
+    if(allowedOrigins.includes(origin) !== -1){
         res.header('Access-Control-Allow-Credentials', true);
     }
     next();
