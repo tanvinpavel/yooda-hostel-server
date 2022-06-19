611ee4e80ac31434b5462c6195d2e399c622dc96
tanvinpavel <tanvinpavel123@gmail.com>
2022-06-19 12:53:31 +0600

    cors slove with true

--- Config/corsOptions.js
+++ Config/corsOptions.js
@@ -1,7 +1,13 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: (origin, callback) => {
+        if(allowedOrigins.indexOf(origin) !== -1 || true){
+            callback(null, true);
+        }else{
+            callback(new Error('not allow by cors'))
+        }
+    },
     credentials: true,
     optionSuccessStatus: 200
 }
